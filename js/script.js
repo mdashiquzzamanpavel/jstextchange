2049f987c66c05e0f81eabed376d53b4cb43ab7a
Md Ashiquzzaman Pavel <94901902+mdashiquzzamanpavel@users.noreply.github.com>
2023-09-02 19:22:16 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// textChange1
+
+function textChange1(){
+	document.getElementById('h2_1').innerHTML="Hello Bangladesh"
+}
+// textChange2
+function textChange2(){
+	document.getElementById('h2_2').innerHTML="Hello USA"
+}
+// textChange3
+function textChange3(){
+	document.getElementById('h2_3').innerHTML="Hello Canada"
+}
+// textChange4
+function textchange4(){
+	document.getElementById('h2_4').innerHTML="Hello Denmark"
+}
+// textChange
+function textChange5(){
+	document.getElementById('h2_5').innerHTML="Hello Finland"
+}
+// textChange
+function textChange6(){
+	document.getElementById('h2_6').innerHTML="Hello Pakistan"
+}
+// textChange
+function textChange7(){
+	document.getElementById('h2_7').innerHTML="Hello Iceland"
+}
+// textChange
+function textChange8(){
+	document.getElementById('h2_8').innerHTML="Hello Brazil"
+}
+// textChange
+function textChange9(){
+	document.getElementById('h2_9').innerHTML="Hello UK"
+}
+// textChange
+function textChange10(){
+	document.getElementById('h2_10').innerHTML="Hello Ireland"
+}
